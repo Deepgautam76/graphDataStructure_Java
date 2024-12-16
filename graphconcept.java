b953dea99301f04c0beea589e51f1449eadc8a7f
Kamal <kamaldeep734833@gmail.com>
2024-12-16 23:00:09 +0530

    Graph Data Structure

---
+++ graphconcept.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+//Here it tells how to create a graph
+//Using an adjacency list
+
+public class graphconcept {
+    static class Edge{
+        int src;
+        int dest;
+        int wait;
+        public Edge(int s,int d,int w){
+            this.dest=d;
+            this.src=s;
+            this.wait=w;
+        }
+    }
+    public static void main(String[] args){
+
+        //Number vertex(v) size of array
+        int V=5;
+        //Create an adjacency list
+        ArrayList<Edge>[] graph=new ArrayList[V];
+
+        for (int i=0; i<V; i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        //0-vertex
+        graph[0].add(new Edge(0,1,5));
+
+        //1-vertex
+        graph[0].add(new Edge(1,0,5));
+        graph[0].add(new Edge(1,2,1));
+        graph[0].add(new Edge(1,3,3));
+
+        //2-vertex
+        graph[0].add(new Edge(2,1,1));
+        graph[0].add(new Edge(2,3,1));
+        graph[0].add(new Edge(2,4,4));
+
+        //3-vertex
+        graph[0].add(new Edge(3,1,3));
+        graph[0].add(new Edge(3,2,1));
+
+        //4-vertex
+        graph[0].add(new Edge(4,2,1));
+
+        //find all neighbors
+        //2's neighbors
+        for(int i=0; i<graph[2].size(); i++){
+            Edge e=graph[2].get(i);
+            //Print all destination value that is the neighbors
+            System.out.println(e.dest);
+        }
+        System.out.println("hello");
+
+    }
+}
